1dc213ca735c9a42f7dcc592d61fceeb90fcaeb5
Malakhow-Alexandr <109951660+Malakhow-Alexandr@users.noreply.github.com>
2023-07-14 12:09:18 +0300

    add initial files

---
+++ src/components/App/App.tsx
@@ -0,0 +1,15 @@
+
+import { GlobalStyle } from "GlobalStyle";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <div>
+        <p>Hello World</p>
+      </div>
+      <GlobalStyle />
+    </div>
+  );
+};
+
+export default App;
